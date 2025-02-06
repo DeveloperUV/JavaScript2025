852773631ef18f70d1aa5dc5975212210af2ce04
Utkarsh Vardhan Sharma <utkarshasharma1311@gmail.com>
2025-02-06 16:51:51 +0000

    variables and datatypes commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "utkarssharma123@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+let accountState;
+
+// accountId = 2; //not allowed
+
+accountEmail = "Rits1234@gmail.com";
+
+accountPassword = 212121;
+
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState ]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treate all JS code as newer version
+
+// alert("hello") // we cannot use Alert in nodejs
+
+let name = "Utkarsh"
+
+let age = 21
+
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>  standalone value
+//undefined 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "utkarsh")
+
+console.log(typeof age)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
