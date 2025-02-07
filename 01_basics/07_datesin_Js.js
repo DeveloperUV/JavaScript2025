6e35445d53b6e5de47485a61ff7778b0ae48c87e
Utkarsh Vardhan Sharma <utkarshasharma1311@gmail.com>
2025-02-07 19:20:45 +0000

    Date and time

---
+++ 01_basics/07_datesin_Js.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2025, 0, 25)
+
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
+
+
