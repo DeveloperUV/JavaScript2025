55c0b7208c847f5d879b154abe02ef40f3a0a340
Utkarsh Vardhan Sharma <utkarshasharma1311@gmail.com>
2025-02-07 08:37:33 +0000

    Datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+// #Primitive Datatype
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+const bigNumber = 1314364898574342312n
+
+
+// Reference Type (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+ let myObj = {
+    name: "utkarsh",
+    age: 21,
+
+ }
+
+ const myFunction = function() {
+    console.log("Hello world");
+    
+ }
+
+ console.log(typeof bigNumber);
+ 
