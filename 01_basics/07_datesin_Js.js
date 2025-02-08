04c0ed27f2af3ce4c0263520a12269f682eb07d9
Utkarsh Vardhan Sharma <utkarshasharma1311@gmail.com>
2025-02-08 06:53:56 +0000

    date and time and methods

--- 01_basics/07_datesin_Js.js
+++ 01_basics/07_datesin_Js.js
@@ -12,7 +12,33 @@ let myDate = new Date()
 
 let myCreatedDate = new Date(2025, 0, 25)
 
-console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
+
+
+
+
+
+
+
+
 
 
 
