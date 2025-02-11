1de2cf9fe71e091eba1ec2e13d3432fba1c53313
Utkarsh Vardhan Sharma <utkarshasharma1311@gmail.com>
2025-02-11 18:11:54 +0000

    continue functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+
+
+function sayMyName() {
+    console.log("U");
+    console.log("T");
+    console.log("K");
+    console.log("A");
+    console.log("R");
+    console.log("S");
+    console.log("H");
+}
+
+
+// sayMyName()
+
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2);
+    
+}
+
+// addTwoNumbers(3, 4)
+
+const result = addTwoNumbers(3, 4)
