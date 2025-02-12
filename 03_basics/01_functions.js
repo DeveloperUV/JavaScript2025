0ac5d0dfcf22c7f65e75618595070d61ac1bc0a3
Utkarsh Vardhan Sharma <utkarshasharma1311@gmail.com>
2025-02-12 16:20:04 +0000

    functions basics part one

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,10 +15,31 @@ function sayMyName() {
 
 
 function addTwoNumbers(number1, number2) {
-    console.log(number1 + number2);
+    // let result = number1 + number2
+    // return result;
+
+    return number1 + number2
     
 }
 
 // addTwoNumbers(3, 4)
 
 const result = addTwoNumbers(3, 4)
+
+// console.log("result: ", result)
+
+function loginUserMessage(username)  {
+    if(username  === undefined) {
+        console.log("Please enter a user name");
+        return
+        
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Utkarsh"))
+console.log(loginUserMessage());
+
+
+
+
